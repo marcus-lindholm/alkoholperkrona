1c673d775b947f4f6054ab16de0744244aa22d9a
marcusxenon <46794636+marcusxenon@users.noreply.github.com>
2024-07-21 19:59:22 +0200

    changed host

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,8 @@ export default function Home({ searchParams }: { searchParams: any }) {
     async function fetchProducts() {
       console.log('fetching products');
       try {
-        const response = await fetch('http://localhost:3000/api/scrapeProducts');
+        //const response = await fetch('http://localhost:3000/api/scrapeProducts');
+        const response = await fetch('https://alkoholperkrona-1nml7gj5b-marcusxenons-projects.vercel.app//api/scrapeProducts');
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
