35abe290fc4ccc10e2fa18e200a945ba09549cf4
marcusxenon <46794636+marcusxenon@users.noreply.github.com>
2024-08-04 00:47:49 +0200

    Improved scraper with type separation + display only the first 20 products

--- app/components/ProductComponent.tsx
+++ app/components/ProductComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type ProductType = {
   id: string;
@@ -15,28 +15,52 @@ type ProductType = {
 };
 
 const ProductComponent = ({ products }: { products: ProductType[] }) => {
+  const [visibleCount, setVisibleCount] = useState(20);
 
   const sortedProducts = products.sort((a, b) => b.apk - a.apk);
+
+  const translateType = (type: string | null) => {
+    if (type == null) {
+      return;
+    } else if (type.toLowerCase() === "beer") {
+      return "Öl";
+    } else if (type.toLowerCase() === "wine") {
+      return "Vin";
+    } else if (type.toLowerCase() === "liquor") {
+      return "Sprit";
+    } else if (type.toLowerCase() === "cider") {
+      return "Cider";
+    }
+    return type;
+  };
+
+  const loadMore = () => {
+    setVisibleCount(prevCount => prevCount + 100);
+  };
 
   return (
     <div className="overflow-x-auto">
       <table className="min-w-full bg-white border border-gray-200">
         <thead>
           <tr>
-          <th className="px-4 py-2 border-b">APK (ml/kr)</th>
+            <th className="px-4 py-2 border-b">Placering</th>
+            <th className="px-4 py-2 border-b">APK (ml/kr)</th>
             <th className="px-4 py-2 border-b">Namn</th>
+            <th className="px-4 py-2 border-b">Typ</th>
             <th className="px-4 py-2 border-b">Pris</th>
             <th className="px-4 py-2 border-b">Volym</th>
             <th className="px-4 py-2 border-b">Volymprocent</th>
           </tr>
         </thead>
         <tbody>
-          {sortedProducts.map((product, index) => (
+          {sortedProducts.slice(0, visibleCount).map((product, index) => (
             <tr key={index} className="hover:bg-gray-100">
+              <td className="px-4 py-2 border-b">{index + 1}</td>
               <td className="px-4 py-2 border-b">{product.apk}</td>
               <td className="px-4 py-2 border-b">
                 <a href={product.url} className="hover:underline"><strong>{product.brand}</strong> {product.name}</a>
               </td>
+              <td className="px-4 py-2 border-b">{translateType(product.type)}</td>
               <td className="px-4 py-2 border-b">{product.price} kr</td>
               <td className="px-4 py-2 border-b">{product.volume} ml</td>
               <td className="px-4 py-2 border-b">{product.alcohol} %</td>
@@ -44,6 +68,11 @@ const ProductComponent = ({ products }: { products: ProductType[] }) => {
           ))}
         </tbody>
       </table>
+      {visibleCount < sortedProducts.length && (
+        <div className="text-center my-4">
+          <button onClick={loadMore} className="px-4 py-2 bg-blue-500 text-white rounded">Visa fler</button>
+        </div>
+      )}
     </div>
   );
 };
--- pages/api/scrapeProducts.ts
+++ pages/api/scrapeProducts.ts
@@ -1,9 +1,11 @@
 import puppeteer from "puppeteer";
 import { PrismaClient } from '@prisma/client';
 
+var beerPages: number;
+var liqourPages: number;
+var winePages: number;
+var ciderPages: number;
 const prisma = new PrismaClient();
-
-//const pupperteer = require('puppeteer');
 const cheerio = require('cheerio');
 
 export default async function handler(req: any, res: any) {
@@ -21,7 +23,8 @@ export default async function handler(req: any, res: any) {
 }
 
 async function runScraper() {
-  console.time('runScraper');
+  console.time('runScraper run time');
+
   /* const browser = puppeteer.launch(
     {
       headless: false,
@@ -50,23 +53,56 @@ async function runScraper() {
   await wait(1500);
   await acceptCookies(page);
   await wait(1000);
-  await navigateSortiment(page);
+  await getNumberOfPages(page);
   await wait(1000);
-  const products = await getProductInfo(page);
 
-  addProductsToDatabase(products).catch(e => {
+  await navigateBeer(page);
+  await wait(2000);
+  const beers = await getProductInfo(page, "beer");
+  addProductsToDatabase(beers).catch(e => {
     throw e
   }).finally(async () => {
     await prisma.$disconnect()
   });
-  console.timeEnd('runScraper');
-  return products;
-  }
+  await wait(1000);
+
+  await navigateLiqour(page);
+  await wait(2000);
+  const liqour = await getProductInfo(page, "liquor");
+  addProductsToDatabase(liqour).catch(e => {
+    throw e
+  }).finally(async () => {
+    await prisma.$disconnect()
+  });
+  await wait(1000);
+
+  await navigateWine(page);
+  await wait(2000);
+  const wine = await getProductInfo(page, "wine");
+  addProductsToDatabase(wine).catch(e => {
+    throw e
+  }).finally(async () => {
+    await prisma.$disconnect()
+  });
+  await wait(1000);
+
+  await navigateCider(page);
+  await wait(2000);
+  const cider = await getProductInfo(page, "cider");
+  addProductsToDatabase(cider).catch(e => {
+    throw e
+  }).finally(async () => {
+    await prisma.$disconnect()
+  });
+
+  console.timeEnd('runScraper run time');
+  //await browser.close();
+  return;
+}
 
 const wait = (ms: any) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
 
 const clickHref = async (page: any) => {
   let counter = 0;
@@ -107,10 +143,104 @@ const acceptCookies = async (page: any) => {
   counter = 3;
 }
 
-const navigateSortiment = async (page: any) => {
+const getNumberOfPages = async (page: any) => {
   let counter = 0;
-  const url = 'https://www.systembolaget.se/sortiment/?p=20';
-  console.log("running navigateSortiment");
+  const url = 'https://www.systembolaget.se/sortiment/';
+  console.log("running getNumberOfPages");
+  const $ = cheerio.load(await page.content());
+
+  while (counter < 3) {
+    try {
+      await page.goto(url, { waitUntil: 'networkidle2' });
+      console.log(`Navigated to ${url}`);
+      const $ = cheerio.load(await page.content());
+      const elements = $('.css-bxnnm4');
+
+      elements.each((index: any, element: any) => {
+        if($(element).find('.css-ziux8h').text() === "Öl") {
+          const beerProducts = parseInt($(element).find('.css-1g5cy76').text().replace(/\s/g, ''));
+          beerPages = Math.ceil(beerProducts / 30);
+        } else if($(element).find('.css-ziux8h').text() === "Vin") {
+          const wineProducts = parseInt($(element).find('.css-1g5cy76').text().replace(/\s/g, ''));
+          winePages = Math.ceil(wineProducts / 30);
+        } else if($(element).find('.css-ziux8h').text() === "Sprit") {
+          const liqourProducts = parseInt($(element).find('.css-1g5cy76').text().replace(/\s/g, ''));
+          liqourPages = Math.ceil(liqourProducts / 30);
+        } else if($(element).find('.css-ziux8h').text() === "Cider & blanddrycker") {
+          const ciderProducts = parseInt($(element).find('.css-1g5cy76').text().replace(/\s/g, ''));
+          ciderPages = Math.ceil(ciderProducts / 30);
+        } else {
+          return;
+        }
+      });
+      
+      return;
+    } catch (error) {
+      console.error(`Failed to navigate, retrying ${counter + 1}`, error);
+      counter++;
+      await wait(1000);
+    }
+  }
+}
+
+const navigateBeer = async (page: any) => {
+  let counter = 0;
+  const url = 'https://www.systembolaget.se/sortiment/ol/?p=' + beerPages;
+  console.log("running navigateBeer");
+
+  while (counter < 3) {
+    try {
+      await page.goto(url, { waitUntil: 'networkidle2' });
+      console.log(`Navigated to ${url}`);
+      return;
+    } catch (error) {
+      console.error(`Failed to navigate, retrying ${counter + 1}`, error);
+      counter++;
+      await wait(1000);
+    }
+  }
+}
+
+const navigateLiqour = async (page: any) => {
+  let counter = 0;
+  const url = 'https://www.systembolaget.se/sortiment/sprit/?p=' + liqourPages;
+  console.log("running navigateLiqour");
+
+  while (counter < 3) {
+    try {
+      await page.goto(url, { waitUntil: 'networkidle2' });
+      console.log(`Navigated to ${url}`);
+      return;
+    } catch (error) {
+      console.error(`Failed to navigate, retrying ${counter + 1}`, error);
+      counter++;
+      await new Promise(resolve => setTimeout(resolve, 1000)); // Use setTimeout for delay
+    }
+  }
+}
+
+const navigateWine = async (page: any) => {
+  let counter = 0;
+  const url = 'https://www.systembolaget.se/sortiment/vin/?p=' + winePages;
+  console.log("running navigateWine");
+
+  while (counter < 3) {
+    try {
+      await page.goto(url, { waitUntil: 'networkidle2' });
+      console.log(`Navigated to ${url}`);
+      return;
+    } catch (error) {
+      console.error(`Failed to navigate, retrying ${counter + 1}`, error);
+      counter++;
+      await new Promise(resolve => setTimeout(resolve, 1000)); // Use setTimeout for delay
+    }
+  }
+}
+
+const navigateCider = async (page: any) => {
+  let counter = 0;
+  const url = 'https://www.systembolaget.se/sortiment/cider-blanddrycker/?p=' + ciderPages;
+  console.log("running navigateCider");
 
   while (counter < 3) {
     try {
@@ -165,53 +295,47 @@ function processVolumeString(input: any) {
   return cleanedInput.replace(' ', '');
 }
 
-const getProductInfo = async (page: any) => {
-  let counter = 0;
+const getProductInfo = async (page: any, type: string) => {
+  await page.waitForSelector('div.css-176nwz9 a', { timeout: 0 });
+
   const $ = cheerio.load(await page.content());
-  const products: { name: any; url: string; price: number; alcohol: number; volume: number; }[] = [];
+  const products: { brand: string; name: any; apk: number; url: string; price: number; alcohol: number; volume: number; type: string }[] = [];
   console.log("running getProductInfo");
 
-  if(!await page.$('div.css-176nwz9 a')) {
-    counter++;
-    console.log("a not found. retrying number: " + counter);
-    if(counter < 3) {
-      await wait(1000);
-      await getProductInfo(page);
-    } else {
-      console.log("a not found");
-    }
-    return;
-  }
+    const aTags = $('div.css-176nwz9 a');
+    aTags.each((index: any, element: any) => {
+      const priceString = $(element).find('.css-1spqwqt .css-1mrpgcx .css-8zpafe .css-6df2t1 .css-vgnpl .css-8zpafe p.css-17znts1').text();
+      const price = parseFloat(processPriceString(priceString).replace(/[^0-9.]/g, ''));
+      const volumeAndAlcohol = $(element).find('.css-1spqwqt .css-1mrpgcx .css-8zpafe .css-6df2t1 .css-vgnpl .css-5aqtg5 p.css-bbhn7t').text();
+      const alcohol = parseFloat(processAlcString(volumeAndAlcohol));
+      const brand = $(element).find('.css-1spqwqt .css-1mrpgcx .css-8zpafe .css-6df2t1 .css-uxm6qc .css-18q0zs4 .css-1n0krvs').text();
+      const name = $(element).find('.css-1spqwqt .css-1mrpgcx .css-8zpafe .css-6df2t1 .css-uxm6qc .css-18q0zs4 .css-123rcq0').text();
+      if (alcohol === 0 || alcohol == null || isNaN(alcohol)) {
+        console.log("Alcohol is 0 or undefined, skipping product " + brand + " " + name);
+        return;
+      }
+      const volume = parseInt(processVolumeString(volumeAndAlcohol));
+      const apk = parseFloat(((alcohol * volume) / (100*price)).toFixed(4));
 
-  const aTags = $('div.css-176nwz9 a');
-  aTags.each((index: any, element: any) => {
-    const priceString = $(element).find('.css-1spqwqt .css-1mrpgcx .css-8zpafe .css-6df2t1 .css-vgnpl .css-8zpafe p.css-17znts1').text();
-    const price = parseFloat(processPriceString(priceString).replace(/[^0-9.]/g, ''));
-    const volumeAndAlcohol = $(element).find('.css-1spqwqt .css-1mrpgcx .css-8zpafe .css-6df2t1 .css-vgnpl .css-5aqtg5 p.css-bbhn7t').text();
-    const alcohol = parseFloat(processAlcString(volumeAndAlcohol));
-    if (alcohol === 0) {
-      return;
-    }
-    const volume = parseInt(processVolumeString(volumeAndAlcohol));
-    const apk = parseFloat(((alcohol * volume) / (100*price)).toFixed(4));
-
-    const product = {
-      brand: $(element).find('.css-1spqwqt .css-1mrpgcx .css-8zpafe .css-6df2t1 .css-uxm6qc .css-18q0zs4 .css-1n0krvs').text(),
-      name: $(element).find('.css-1spqwqt .css-1mrpgcx .css-8zpafe .css-6df2t1 .css-uxm6qc .css-18q0zs4 .css-123rcq0').text(),
-      apk: apk,
-      url: "https://systembolaget.se" + $(element).attr('href'),
-      price: price,
-      alcohol: alcohol,
-      volume: volume,
-    }
-    products.push(product);
-    //console.log(product);
-  })
+      const product = {
+        brand: brand,
+        name: name,
+        apk: apk,
+        url: "https://systembolaget.se" + $(element).attr('href'),
+        price: price,
+        alcohol: alcohol,
+        volume: volume,
+        type: type,
+      }
+      products.push(product);
+      //console.log(product);
+    })
   return products;
 }
 
 async function addProductsToDatabase(products: any) {
   if (!Array.isArray(products)) {
+    console.log(products);
     console.error('Error: products is not an array. Type:', typeof products);
     return;
   }
