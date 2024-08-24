2db3db3bb0a31152c25975de32734f6605cfcaad
Marcus Lindholm <hello@marcuslindholm.com>
2024-08-24 12:29:08 +0200

    adSense component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+import Adsense from "./components/AdSense";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,13 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <script 
-          async 
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8117821348663742"
-          crossOrigin="anonymous">
-        </script>
-      </Head>
+      <head>
+        <Adsense pId="8117821348663742" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
