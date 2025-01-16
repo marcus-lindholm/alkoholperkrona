f6dff1542532a8055cf258947b6d2258461c2baa
Marcus Lindholm <hello@marcuslindholm.com>
2025-01-16 15:14:36 +0100

    removed image again

--- app/components/ProductComponentMobile.tsx
+++ app/components/ProductComponentMobile.tsx
@@ -86,7 +86,7 @@ const ProductComponentMobile = ({ products = [], isDarkMode, isBeastMode, showDe
                   </div>
               </a>
             </div>
-            <div className="grid grid-cols-3 gap-1">
+            <div className="grid grid-cols-2 gap-1">
               <div>
                   <div className="mb-4">
                       <span className="text-sm opacity-85">APK</span><br/>
@@ -98,7 +98,7 @@ const ProductComponentMobile = ({ products = [], isDarkMode, isBeastMode, showDe
                       <span className="text-2xl font-bold">{priceFormatted} kr</span>
                   </div>
               </div>
-              <div className='flex items-center justify-center'>
+              {/* <div className='flex items-center justify-center'>
               {product.img ? (
                 <ProductImage 
                   url={product.url} 
@@ -109,7 +109,7 @@ const ProductComponentMobile = ({ products = [], isDarkMode, isBeastMode, showDe
               ) : (
                 <div className='w-24 h-24 rounded bg-transparent flex items-center justify-center'></div>
               )}
-              </div>
+              </div> */}
               <div className='items-right text-right'>
                 <div className="mb-2 whitespace-nowrap">
                   <a href={product.url} target="_blank" rel="noopener noreferrer" className="hover:underline">
