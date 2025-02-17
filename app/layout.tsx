5f08cab338aea6c3ac081d1786bee91d2cea6611
Marcus Lindholm <hello@marcuslindholm.com>
2025-02-17 10:28:00 +0100

    commented adsense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,11 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:image" content="/favicon.ico" />
         <meta property="og:url" content="https://www.apkrona.se" />
-        <script 
+        {/* <script 
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8117821348663742"
           crossOrigin="anonymous">
-        </script>
+        </script> */}
       </Head>
       <body className={inter.className}>
         {children}
