161abf84ff9bcdb60cd74cce18743ab32bea516c
Marcus Lindholm <hello@marcuslindholm.com>
2026-02-21 20:51:37 +0100

    Fixed Clarity bug that only allowed recordings on home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../app/globals.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script id="clarity-script" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+        `}
+      </Script>
+    </>
+  );
 }
 
 export default MyApp;
